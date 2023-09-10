532e5068eb316805adf67f6a57115dd25fb2737b
ajSL17 <zero_gmz@outlook.com>
2023-09-10 10:30:21 -0500

    añadido a git

---
+++ src/main/java/pe/edu/cibertec/appwebpractica1/AppWebPractica1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebpractica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebPractica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebPractica1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebpractica1/AppWebPractica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebpractica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebPractica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
